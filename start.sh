5f477c61a5d6e5cd79f28e0f5f072febe3e9798f
achukuttanTG <104365884+achukuttanTG@users.noreply.github.com>
2022-07-14 03:18:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/DKBOTx/private-bot.git /private-bot
+  git clone https://github.com/achukuttanTG/MusicFilter.git /MusicFilter
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /private-bot
+  git clone $UPSTREAM_REPO /MusicFilter
 fi
-cd /private-bot
+cd /MusicFilter
 pip3 install -U -r requirements.txt
 echo "sᴛᴀʀᴛɪɴɢ 𝗗𝗞 𝗕𝗢𝗧𝘅........"
 python3 bot.py
